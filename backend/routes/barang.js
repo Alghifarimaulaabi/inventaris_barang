7ec6a6c8f41570fa5276d20107c380a368df3998
Alghifari Maula Abi <alghifarimaulaabi@gmail.com>
2026-05-30 15:23:10 +0700

    fix dashboard user

--- backend/routes/barang.js
+++ backend/routes/barang.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Barang } = require('../models');
+const { Barang, sequelize } = require('../models');
 const { authenticateToken } = require('../middleware/auth');
 const multer = require('multer');
 const path = require('path');
